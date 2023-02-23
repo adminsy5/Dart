db69155dec1b19a4df5f849e1fabc49d159170ea
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-23 12:41:27 +0530

    ok

--- PerformListOpe.dart
+++ PerformListOpe.dart
@@ -31,12 +31,33 @@ void main() {
   print("\n--> List Is not empty : ${LstInt.isNotEmpty}");
   print("\n--> First Object of First List : ${LstInt.first}");
   print("\n--> Last Object of Last List : ${LstInt.last}");
-
-  //Todo
-  //Insert
-  //Update
-  //Delete
-  //Replace
-  //Add
-  //Or more Goto Google !
+  print("\n Before add New Item !\n");
+  print(LstInt);
+  print("\n After Added New Item ! .Add\n");
+  LstInt.add(999);
+  print(LstInt);
+  LstInt.addAll([001, 002, 003, 004, 005]);
+  print("\n--> AddAll : $LstInt");
+  LstInt.insert(3, 5655);
+  print("\n--> Insert : $LstInt");
+  LstString.insertAll(3, ["Divya", "Sweta", "Anjali", "Anki"]);
+  print("\n--> InsertAll : $LstString");
+  LstString.remove("Sweta");
+  print("\n--> Remove : $LstString");
+  LstString.removeAt(4);
+  print("\n--> remove At 4th Position : $LstString");
+  LstString.removeLast();
+  print("\n--> Remove Last : $LstString");
+  LstString.removeRange(1, 4);
+  print("\n--> Remove range : $LstString");
+  LstString.removeWhere((element) => LstString.length == 1);
+  print("\n--> Remove where : $LstString");
+  var int1 = Mixed.whereType<int>();
+  print("\n--> Integer from MIxed List : $int1");
+  var String1 = Mixed.whereType<String>();
+  print("\n--> String from MIxed List : $String1");
+  var float1 = Mixed.whereType<double>();
+  print("\n--> Float from MIxed List : $float1");
+  LstInt.replaceRange(1, 5, [786]);
+  print("\n--> Replace Range : $LstInt");
 }
